4d1de2aec58ef3490bb686886eda14fa64ac86c9
Ethan Ma <ethanmaisawesome@gmail.com>
2023-04-23 22:42:37 -0700

    feat: better documentation

--- TrainingControlCenter/backend/src/strava_api.py
+++ TrainingControlCenter/backend/src/strava_api.py
@@ -1,19 +1,54 @@
+"""Strava API Calls Manager
+
+This script allows easy connection between the user data by storing all data
+into json files.
+
+This file can be imported as a module and contains the following class:
+
+    * stravaAPI - class for strava api
+"""
+__author__ = "Ethan Ma"
+
 import requests
 import os
 import json
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('TOKEN') # TBD
 HEADER = {'Authorization': f"Bearer {TOKEN}"}
 JSON_DIR = "./data"
 
 
-class Strava_API:
+class stravaAPI:
+    """
+    A class used to manage the Strava API.
+    ...
 
-    def createJSON(self, data, name):
+    Methods
+    ----
+    get_all_athlete_activities()
+    get_athlete_stats()
+    get_athlete_zones()
+    get_clubs()
+    get_starred_segments()
+    """
+
+    def _create_json(self, data, name):
+        """Helper function to write json data to JSON_DIR.
+
+        :param data (list): list of data from api call
+        :param name (str): name of file to be created
+        :return:
+        """
         with open(f"{JSON_DIR}/{name}.json", 'w') as f:
             json.dump(data, f, indent=4)
 
-    def getAllAthleteActivities(self):
+    def get_all_athlete_activities(self):
+        """Gathers data for all of athlete's activities.
+
+        This displays a list of ALL the user's activities. Each of these
+        activities have various data points such as activity name, elapsed time,
+        activity type, and more.
+        """
         name = "all_athlete_activities"
         activites_url = "https://www.strava.com/api/v3/athlete/activities"
         param = {'per_page': 200, 'page': 1}
@@ -23,21 +58,28 @@ class Strava_API:
             all_data.append(data)
             param['page'] += 1
             data = requests.get(activites_url, headers=HEADER, params=param).json()
-        self.createJSON(all_data, name)
+        self._create_json(all_data, name)
 
-    def getAthleteStats(self):
+    def get_athlete_stats(self):
+        """Gathers data for all of athlete's stats.
+
+        This displays the users profile page information, such as username,
+        account creation, follower counts, and more.
+        """
         name = "athlete_stats"
         activities_url = "https://www.strava.com/api/v3/athlete"
         data = requests.get(activities_url, headers=HEADER).json()
-        self.createJSON(data, name)
+        self._create_json(data, name)
 
-    def getAthleteZones(self):
+    def get_athlete_zones(self):
+        """Gathers data for the athlete's heart rate and power zones."""
         name = "athlete_zones"
         activities_url = "https://www.strava.com/api/v3/athlete/zones"
         data = requests.get(activities_url, headers=HEADER).json()
-        self.createJSON(data, name)
+        self._create_json(data, name)
 
-    def getClubs(self):
+    def get_clubs(self):
+        """Gathers data for all of athlete's clubs."""
         name = "all_athlete_clubs"
         activites_url = "https://www.strava.com/api/v3/athlete/clubs?page=&per_page="
         param = {'per_page': 30, 'page': 1}
@@ -47,9 +89,10 @@ class Strava_API:
             all_data.append(data)
             param['page'] += 1
             data = requests.get(activites_url, headers=HEADER, params=param).json()
-        self.createJSON(all_data, name)
+        self._create_json(all_data, name)
 
-    def getStarredSegments(self):
+    def get_starred_segments(self):
+        """Gathers data for all of athlete's starred segments."""
         name = "all_starred_segments"
         activites_url = "https://www.strava.com/api/v3/segments/starred?page=&per_page="
         param = {'per_page': 30, 'page': 1}
@@ -59,4 +102,4 @@ class Strava_API:
             all_data.append(data)
             param['page'] += 1
             data = requests.get(activites_url, headers=HEADER, params=param).json()
-        self.createJSON(all_data, name)
+        self._create_json(all_data, name)
