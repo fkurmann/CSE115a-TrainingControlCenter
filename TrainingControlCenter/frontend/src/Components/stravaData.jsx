b86bc8e28f6fbd5c8f47ae327a3f131182b99214
Ethan Ma <etma@cisco.com>
2023-05-02 13:46:49 -0700

    feat: working upload for all and five activities

--- TrainingControlCenter/frontend/src/Components/stravaData.jsx
+++ TrainingControlCenter/frontend/src/Components/stravaData.jsx
@@ -1,89 +1,99 @@
 import axios from 'axios';
-import React from 'react';
 
 const stravaBaseURL = 'https://www.strava.com/api/v3';
 
-export async function getAllActivities() {
-    const user = localStorage.getItem('user');
-    const stravaAccessToken = localStorage.getItem('stravaAccessToken');
-
-    var all_activities = [{}];
+async function uploadActivities(activities) {
+  const user = localStorage.getItem('user');
+  for (let i = 0; i < activities.length; i++) {
     try {
-        var page = 1;
-        var res = await axios.get(`${stravaBaseURL}/athlete/activities`, {
-            headers: {
-                'Authorization': `Bearer ${stravaAccessToken}`,
-            },
-            params: {
-                'page': page,
-                'per_page': 200,
-            },
-        });
-        if (res.status === 200) {
-            all_activities += await res.data;
-        }
-        while (res.length !== 0) {}
-            page += 1;
-            res = await axios.get(`${stravaBaseURL}/athlete/activities`, {
-                headers: {
-                    'Authorization': `Bearer ${stravaAccessToken}`,
-                },
-                params: {
-                    'page': page,
-                    'per_page': 200,
-                },
-            });
-            if (res.status === 200) {
-                all_activities += res.data;
-            }
+      const res = await axios.post('http://localhost:3010/v0/activities',
+        {
+          username: JSON.stringify(user),
+          name: JSON.stringify(activities[i]['id']),
+          sport: JSON.stringify(activities[i]['sport_type']),
+          type: JSON.stringify(activities[i]['type']),
+          json: activities[i],
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        },
+      );
+      if (res.status === 200) {
+        continue;
+      }
     } catch (error) {
-        console.error('Error fetching all activities:', error);
-        return null;
+      console.error('Error posting activity', activities[i], error);
+      return null;
     }
-    return all_activities; // here should be uploading to DB not return.
+  }
+  console.log("Stored activities for: ", user);
+}
+
+export async function getAllActivities() {
+  const stravaAccessToken = localStorage.getItem('stravaAccessToken');
+
+  var all_activities = [];
+  try {
+    var page = 1;
+    var res = await axios.get(`${stravaBaseURL}/athlete/activities`, {
+      headers: {
+        'Authorization': `Bearer ${stravaAccessToken}`,
+      },
+      params: {
+        'page': page,
+        'per_page': 100,
+      },
+    });
+    if (res.status === 200) {
+      all_activities = res.data;
+    }
+    while (res.length !== 0) {}
+      page += 1;
+      res = await axios.get(`${stravaBaseURL}/athlete/activities`, {
+        headers: {
+          'Authorization': `Bearer ${stravaAccessToken}`,
+        },
+        params: {
+          'page': page,
+          'per_page': 200,
+        },
+      });
+      if (res.status === 200) {
+        for (let i = 0; i < res.data.length; i++) {
+          all_activities.push(res.data[i]);
+        }
+      }
+  } catch (error) {
+    console.error('Error fetching all activities:', error);
+    return null;
+  }
+  uploadActivities(all_activities)
+  return all_activities; // here should be uploading to DB not return.
 }
 
 export async function getFiveActivities() {
-    const user = localStorage.getItem('user');
-    const stravaAccessToken = localStorage.getItem('stravaAccessToken');
-    var activities = [{}];
-    try {
-        const res = await axios.get(`${stravaBaseURL}/athlete/activities`, {
-            headers: {
-                'Authorization': `Bearer ${stravaAccessToken}`,
-            },
-            params: {
-                'per_page': 5,
-            },
-        });
-        if (res.status === 200) {
-            activities += res;
-        }
-    } catch (error) {
-        console.error('Error fetching recent 5 activities:', error);
-        return null;
+  const stravaAccessToken = localStorage.getItem('stravaAccessToken');
+  var activities = [];
+  try {
+    const res = await axios.get(`${stravaBaseURL}/athlete/activities`, {
+      headers: {
+        'Authorization': `Bearer ${stravaAccessToken}`,
+      },
+      params: {
+        'per_page': 5,
+      },
+    });
+    if (res.status === 200) {
+      for (let i = 0; i < res.data.length; i++) {
+        activities.push(res.data[i]);
+      }
     }
-    for (let i = 0; i < activities.length; i++) {
-        try {
-            const res = await axios.post('http://localhost:3010/v0/activities', {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: {
-                    username: user,
-                    name: activities[i]['id'],
-                    sport: activities[i]['sport_type'],
-                    type: activities[i]['type'],
-                    json: activities[i],
-                },
-            });
-            if (res.status === 200) {
-                continue;
-            }
-        } catch (error) {
-            console.error('Error posting activity', activities[i], error);
-            return null;
-        }
-    }
-    console.log("Stored activities for: ", user);
+  } catch (error) {
+    console.error('Error fetching recent 5 activities:', error);
+    return null;
+  }
+  uploadActivities(activities);
+  return activities;
 }
