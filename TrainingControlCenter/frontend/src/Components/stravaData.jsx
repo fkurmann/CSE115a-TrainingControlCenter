a5229c455aa87865e4c34d36736510ef749ea711
Ethan Ma <etma@cisco.com>
2023-05-02 01:08:58 -0700

    feat: upload to 5 activities to db and error fixing

--- TrainingControlCenter/frontend/src/Components/stravaData.jsx
+++ TrainingControlCenter/frontend/src/Components/stravaData.jsx
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import React from "react";
+import React from 'react';
 
 const stravaBaseURL = 'https://www.strava.com/api/v3';
 
-export default function getAllActivities = async () => {
+export const getAllActivities = async () => {
     const user = localStorage.getItem('user');
     const stravaAccessToken = localStorage.getItem('stravaAccessToken');
 
     var all_activities = [{}];
     try {
-        page = 1;
+        var page = 1;
         var res = await axios.get(`${stravaBaseURL}/athlete/activities`, {
             headers: {
                 'Authorization': `Bearer ${stravaAccessToken}`,
@@ -43,7 +43,7 @@ export default function getAllActivities = async () => {
     return all_activities; // here should be uploading to DB not return.
 }
 
-export default function getFiveActivities = async () => {
+export const getFiveActivities = async () => {
     const user = localStorage.getItem('user');
     const stravaAccessToken = localStorage.getItem('stravaAccessToken');
     var activities = [{}];
@@ -63,5 +63,27 @@ export default function getFiveActivities = async () => {
         console.error('Error fetching recent 5 activities:', error);
         return null;
     }
-    return activites;
+    for (let i = 0; i < activities.length; i++) {
+        try {
+            const res = await axios.post('localhost:3010/activities/', {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: {
+                    username: user,
+                    name: activities[i]['id'],
+                    sport: activities[i]['sport_type'],
+                    type: activities[i]['type'],
+                    json: activities[i],
+                },
+            });
+            if (res.status === 200) {
+                continue;
+            }
+        } catch (error) {
+            console.error('Error posting activity', activities[i]);
+            return null;
+        }
+    }
+    return activities;
 }
