2cee9b39fc0090702a124c6ea6efe58618ee19fe
Fabrice Kurmann <74742714+fkurmann@users.noreply.github.com>
2023-05-16 15:27:47 -0700

    Activities chart (#17)
    * initial graph for ride/run distance with 3 duration filters
    
    * initial graph for run/ride distance w/ fixed duration
    
    * graph time w/ week, month, year
    
    * list sort w/ week, month, year
    
    * add selection buttons, dropdown lists, rankings, and charts
    
    * add activitiesCharts to DataCenter page
    
    ---------
    
    Co-authored-by: DESKTOP-8DUS6P4\tongl <ylin160@ucsc.edu>

--- TrainingControlCenter/frontend/src/Pages/DataCenter.jsx
+++ TrainingControlCenter/frontend/src/Pages/DataCenter.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import ResponsiveAppBar from '../Components/appBar';
+import ActivitiesChart from '../Components/activitiesChart'; // import starActivities
 
 /**
  * Create the theme to be used
@@ -22,10 +23,11 @@ export default function DataCenter() {
         <ResponsiveAppBar />
         <h1>Training Control Center, {localStorage.getItem('user')}: Data Center</h1>
         <Box>
+          <ActivitiesChart/>
         </Box>
-        
+
       </Container>
-      
+
     </ThemeProvider>
   );
 }
