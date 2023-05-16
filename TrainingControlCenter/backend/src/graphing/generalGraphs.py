36d8d192a1ec29ab1459cbd486aa33ac7b072f62
fkurmann <fabrice.kurmann@gmail.com>
2023-05-16 08:56:33 -0700

    Graphing producing informative graphs, need to work on variants and detaisl

--- TrainingControlCenter/backend/src/graphing/generalGraphs.py
+++ TrainingControlCenter/backend/src/graphing/generalGraphs.py
@@ -5,10 +5,13 @@ import numpy as np
 import matplotlib.image as mplimg
 import math
 import sys
+import json
 
-def generalHistoryGraph (username, duration, graphType, sport, goal, startDate, outFile):
+from datetime import date, timedelta
 
-  print ('Params: ' + username, duration, graphType, sport, goal, startDate, outFile)
+def generalHistoryGraph (username, duration, graphType, sport, goal, startYear, startMonth, startDay, jsonInput, outFile):
+
+  print ('Params: ' + username, duration, graphType, sport, goal, startYear, startMonth, startDay, jsonInput, outFile)
   # Figure creation
   figureWidth=5
   figureHeight=5
@@ -20,32 +23,63 @@ def generalHistoryGraph (username, duration, graphType, sport, goal, startDate, 
 
   panel1 = plt.axes([0.5/figureWidth, 0.5/figureHeight, panelWidth/figureWidth,panelHeight/figureHeight])
 
+  # Database date filtering
+  # minDate = date(startYear, startMonth, startDay) 
+  # maxDate = minDate
+  # if duration == 'Day':
+  #   maxDate = minDate + timedelta(days=14)
+
+  # elif duration == 'Week':
+  #   maxDate = minDate + timedelta(weeks=12)
+  # else:
+  #   maxDate = minDate + timedelta(months=12)
+
+
+  activityList = json.loads(jsonInput)
+  # print(activityDict)
+
+  for item in activityList:
+    print (item['name'])
+
+  # Get totals for distance or time
+  totals = []
+  counter = 1
+  if (graphType == 'Distance'):
+    # Daily total graphing
+
+    # Weekly total graphing
+
+    # Monthly total graphing
+    
+    for item in activityList:
+      totals.append((counter, item['distance']/1609.34))
+      counter += 1
+  if (graphType == 'Time'):
+    for item in activityList:
+      totals.append((counter, item['moving_time']/3600))
+      counter += 1
+    
+  
+    # Daily total graphing
+
+    # Weekly total graphing
+
+    # Monthly total graphing
+
+
+
+
+  
+
+
+
 
   # Graphing
-  maxY = 10 # maxY is the largest y value, can be goal value
-  maxY = math.ceil(maxY * 1.1)
-
-  # Data Graphing TODO
-
-
-
-
-
-
-  # Goal overlay, optional
-  if goal == True:
-    pass
-
-
-
-
-
-
-
-
-
-          
   # Panel ranges
+  maxY = max(totals)[1] # maxY is the largest y value, can be goal value
+  maxY = math.ceil(maxY * 1.1)
+  yTickSpace = maxY//10
+
   maxX = 13
   if duration == 'Day':
     maxX = 15
@@ -53,12 +87,32 @@ def generalHistoryGraph (username, duration, graphType, sport, goal, startDate, 
   panel1.set_xlim(0, maxX)
   panel1.set_ylim(0, maxY) 
 
+  
+
+  for item in totals:
+    panel1.plot(item[0],item[1],
+                  marker='o',
+                  markerfacecolor='red',
+                  markeredgewidth=0.1,
+                  markersize=4,
+                  color='black',
+                  linewidth=0,
+                  alpha=1
+      )
+
+
+  # If goals selected, get goal for corresponding sport of type time/distance from database TODO
+
+  # Goal overlay, optional
+  if goal == True:
+    pass
+
   # Ticks and labels 
   panel1.set_xticks(np.arange(0, maxX))
-  panel1.set_yticks(np.arange(0, maxY))
+  panel1.set_yticks(np.arange(0, maxY, yTickSpace))
 
   # Title
-  # panel1.set_title(graphType + ' per ' duration + ' for all Sports')
+  panel1.set_title(graphType + ' per ' + duration + ' for ' + sport)
 
   panel1.set_xlabel(duration + 's')
   panel1.set_ylabel(graphType)
@@ -71,8 +125,8 @@ def generalHistoryGraph (username, duration, graphType, sport, goal, startDate, 
 
   # TODO, save location in images folder
   plt.savefig('../frontend/images/' + outFile,dpi=600)
-  # plt.savefig(outFile,dpi=600)
+  #plt.savefig(outFile,dpi=600)
   return 'Graphing Complete'
 
-
-generalHistoryGraph(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+# generalHistoryGraph('fkurmann', 'Week', 'Distance', 'Run', False, 2023, 2, 19, 'string', 'test')
+generalHistoryGraph(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10])
--- TrainingControlCenter/backend/src/graphs.js
+++ TrainingControlCenter/backend/src/graphs.js
@@ -1,3 +1,4 @@
+const dbActivities = require('../db/dbActivities');
 const {spawn} = require('child_process');
 
 // General graphing function
@@ -5,15 +6,46 @@ exports.drawGraph = async (req, res) => {
   // Parameters
   let {username, duration, graphType, sport, goal, startDate, outFile} = req.body;
   
-  
+  // Format date
+  console.log(startDate)
+  startDate = new Date(startDate);
+  console.log(startDate)
+
+  // Set end date for DB searching
+  let endDate = new Date(startDate);
+  if (duration === 'Day'){
+    endDate = endDate.setDate(endDate.getDate() + 14);
+    endDate = new Date(endDate);
+  } else if (duration === 'Week'){
+    endDate = endDate.setDate(endDate.getDate() + 84);
+    endDate = new Date(endDate);
+  } else {
+    endDate = endDate = endDate.setFullYear(endDate.getFullYear() + 1)
+    endDate = new Date(endDate);
+  }
+
+  // Convert the date object to a YYYY/MM/DD format
+  let formattedStartDate = startDate ? startDate.toISOString().substring(0, 10) : null;  
+  let formattedEndDate = endDate ? endDate.toISOString().substring(0, 10) : null;
+
+  console.log(formattedStartDate)
+  console.log(formattedEndDate)
+
+  // Database call
+  const returnValue = await dbActivities.findActivity(username, null, sport, null, null, null, null, null, formattedStartDate, formattedEndDate);
+  // Error case
+  if (returnValue === -1) {
+    res.status(401).send('Error getting activities from graph.js');
+  } 
+
   // Spawn python graphing child processs
   let responseData;
-  const python = await spawn('python3', ['./src/graphing/generalGraphs.py', username, duration, graphType, sport, goal, startDate, outFile]);
+  const python = await spawn('python3', ['./src/graphing/generalGraphs.py', username, duration, graphType, sport, goal, startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), JSON.stringify(returnValue), outFile]);
   
   // Stdout data
   python.stdout.on('data', (data) => {
     responseData = data.toString();
-    console.log('stdout: ' + data);
+    //console.log('stdout: ' + data);
   });
   
   // Error data
@@ -25,6 +57,7 @@ exports.drawGraph = async (req, res) => {
   python.on('close', (code) => {
     console.log('Closing: ' + code);
     console.log(responseData)
-    res.send(responseData)
+    //res.send(responseData)
+    res.status(200).json(returnValue);
   });
 };
