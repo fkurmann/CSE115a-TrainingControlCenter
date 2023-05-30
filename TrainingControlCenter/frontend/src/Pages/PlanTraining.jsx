febbf746c53771c03d56c2822155cd09500034cf
DESKTOP-8DUS6P4\tongl <ylin160@ucsc.edu>
2023-05-30 14:39:01 -0700

    Display total distance and total time for the selected date range

--- TrainingControlCenter/frontend/src/Pages/PlanTraining.jsx
+++ TrainingControlCenter/frontend/src/Pages/PlanTraining.jsx
@@ -7,6 +7,7 @@ import Box from '@mui/material/Box';
 import ResponsiveAppBar from '../Components/appBar';
 import { userTheme } from '../Components/theme';
 import AddPlannedActivityForm from '../Components/addPlannedActivityForm';
+import AddPlannedSummary from '../Components/addPlannedSummary';
 
 /**
  * Creates the Training Plan page for workout creation and planning.
@@ -22,6 +23,7 @@ export default function PlanTraining() {
         <ResponsiveAppBar />
         <h1>Training Control Center, {localStorage.getItem('user')}: Plan Training</h1>
         <Box>
+          <AddPlannedSummary />
           <AddPlannedActivityForm />
         </Box>
       </Container>
